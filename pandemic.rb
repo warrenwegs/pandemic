c96ce36fb69e5d8c78764e281ba3475f832071e1
Warren Wegner <warrenwegner@gmail.com>
2016-01-19 21:25:42 -0800

    added city class

--- pandemic.rb
+++ pandemic.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'yaml'
 
-get '/' do
-  city = YAML::load_file(File.join(__dir__, 'cities.yml'))
+class City
+  attr_reader :name, :color, :adjCities
+  def initialize(name, color, connectedCities)
+    @name = name
+    @color = color
+    @adjCities = connectedCities
+  end
 
-  erb :cities, locals: { cities: city }
+end
+
+citiesData = YAML::load_file(File.join(__dir__, 'cities.yml'))
+
+cities = Array.new
+
+citiesData.each do |name, properties|
+  city = City.new(name, properties['Color'], properties['Connected'])
+  cities << city
+end
+
+get '/' do
+  erb :cities, locals: { cities: cities }
 end
