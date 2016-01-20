fb3b8bdf8ebc452cb3e3c39238755abfd4ee4a2e
Warren Wegner <warrenwegner@gmail.com>
2016-01-19 22:31:58 -0800

    adds city location data

--- pandemic.rb
+++ pandemic.rb
@@ -2,11 +2,13 @@ require 'sinatra'
 require 'yaml'
 
 class City
-  attr_reader :name, :color, :adjCities
-  def initialize(name, color, connectedCities)
+  attr_reader :name, :color, :adjCities, :x, :y
+  def initialize(name, color, connectedCities, x, y)
     @name = name
     @color = color
     @adjCities = connectedCities
+    @x = x
+    @y = (y - 15) * 1.7
   end
 
 end
@@ -16,7 +18,7 @@ citiesData = YAML::load_file(File.join(__dir__, 'cities.yml'))
 cities = Array.new
 
 citiesData.each do |name, properties|
-  city = City.new(name, properties['Color'], properties['Connected'])
+  city = City.new(name, properties['Color'], properties['Connected'], properties['Coords']['x'], properties['Coords']['y'])
   cities << city
 end
 
