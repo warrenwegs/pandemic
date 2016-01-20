ad7489a308476b9dbdf8349c65a8f671316317b4
Warren Wegner <warrenwegner@gmail.com>
2016-01-19 22:55:38 -0800

    links city objects together

--- pandemic.rb
+++ pandemic.rb
@@ -2,7 +2,8 @@ require 'sinatra'
 require 'yaml'
 
 class City
-  attr_reader :name, :color, :adjCities, :x, :y
+  attr_reader :name, :color, :x, :y
+  attr_accessor :adjCities
   def initialize(name, color, connectedCities, x, y)
     @name = name
     @color = color
@@ -10,17 +11,28 @@ class City
     @x = x
     @y = (y - 15) * 1.7
   end
+end
 
+def linkCities(cities)
+  cities.each do |name, city|
+    adjCityObj = Array.new
+    city.adjCities.each do |adjCity|
+      adjCityObj << cities[adjCity]
+    end
+    city.adjCities = adjCityObj
+  end
 end
 
 citiesData = YAML::load_file(File.join(__dir__, 'cities.yml'))
 
-cities = Array.new
+cities = Hash.new
 
 citiesData.each do |name, properties|
   city = City.new(name, properties['Color'], properties['Connected'], properties['Coords']['x'], properties['Coords']['y'])
-  cities << city
+  cities[city.name] = city
 end
+
+linkCities(cities)
 
 get '/' do
   erb :cities, locals: { cities: cities }
